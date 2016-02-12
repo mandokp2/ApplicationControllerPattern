ddd3b65b4e9dd953c2e04551c5902f8db3410977
mandokp2 <lop12032@byui.edu>
2016-02-11 18:11:53 -0700

    Create TortaHandler.java

---
+++ TortaHandler.java
@@ -0,0 +1,34 @@
+package appCont;
+
+import JSON.FileReader;
+
+import java.text.NumberFormat;
+import java.util.HashMap;
+
+/**
+ * Created by newuser on 2/8/16.
+ */
+public class TortaHandler implements AChandler {
+    public void handleIt(HashMap data)  {
+        try {
+            FileReader fileReader = new FileReader();
+            HashMap d1 = fileReader.readFile();
+
+            System.out.println("Great Choice");
+            System.out.println(d1.get("Tortas"));
+            HashMap d2 = (HashMap) data;
+            int numberAmount = (Integer) d2.get("amount");
+            double totalAmount = numberAmount * (Double) d2.get("tortaPrice");
+
+            NumberFormat formatter = NumberFormat.getCurrencyInstance();
+            System.out.println("Your total amount is " + formatter.format(totalAmount));
+
+            System.out.println("Tortas are Delicious!");
+
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+
+
+    }
+}
